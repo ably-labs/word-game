3645e47183558d32defd6e3e5d06af94470f118d
Peter Maguire <peter.maguire@ably.com>
2022-02-04 11:51:06 +0000

    feat: allow lobby owner to skip the current player

--- word-game-be/middleware/Game.go
+++ word-game-be/middleware/Game.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/ably-labs/word-game/word-game-be/entity"
 	"github.com/ably-labs/word-game/word-game-be/model"
 	"github.com/labstack/echo/v4"
@@ -11,8 +10,7 @@ func RequireTurn(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		lobby := c.Get("lobby").(*model.Lobby)
 		user := c.Get("user").(*model.User)
-		if *lobby.PlayerTurnID != *user.ID {
-			fmt.Println(lobby.PlayerTurnID, user.ID)
+		if *lobby.PlayerTurnID != *user.ID && *lobby.CreatorID != *user.ID {
 			return c.JSON(403, entity.ErrNotYourTurn)
 		}
 		return handlerFunc(c)
